c0d9eeb70a769cedeb58bc5319a7262ca46851e1
MolybdenumLee <limu.robert@outlook.com>
2019-10-09 14:34:08 +0800

    modifications on cmd interface

--- AutoCONTIN_cmd.py
+++ AutoCONTIN_cmd.py
@@ -14,35 +14,38 @@ def singleMode():
         print('Ooops, something went wrong...')
 
 def batchMode():
-    data_folder = input('\ndata folder (absolute path recommended): ')
-    print('\n'+'='*20 + ' AutoCONTIN by LiMu ' + '='*20 + '\n')
-    file_list = os.listdir(data_folder)
+    data_folder = input('data folder (absolute path recommended): ')
+    try:
+        file_list = os.listdir(data_folder)
+        print('\n'+'='*20 + ' AutoCONTIN by LiMu ' + '='*20 + '\n')
 
-    print('-----------------------------------------------')
-    print('        Result        |      data file name')
-    print('----------------------+------------------------')
-    N_datafile = 0
-    N_success = 0
-    N_fail = 0
-    for f in file_list:
-        if f.split('.')[-1] == 'dat':
-            N_datafile += 1
-            try:
-                datafile_abspath = os.path.join(data_folder, f)
-                autoContin(datafile_abspath, full_auto=True, rename=True)
-                print(' - O - CONTIN success | {}'.format(f))
-                N_success += 1
-            except:
-                print(' - X - CONTIN fail    | {}'.format(f))
-                N_fail += 1
-    print('-----------------------------------------------')
-    print('all done, {} in total, {} success, {} fail. Have a nice day!\n'.format(N_datafile, N_success, N_fail))
-    #print('failed file:')
+        print('-----------------------------------------------')
+        print('        Result        |      data file name')
+        print('----------------------+------------------------')
+        N_datafile = 0
+        N_success = 0
+        N_fail = 0
+        for f in file_list:
+            if f.split('.')[-1] == 'dat':
+                N_datafile += 1
+                try:
+                    datafile_abspath = os.path.join(data_folder, f)
+                    autoContin(datafile_abspath, full_auto=True, rename=True)
+                    print(' - O - CONTIN success | {}'.format(f))
+                    N_success += 1
+                except:
+                    print(' - X - CONTIN fail    | {}'.format(f))
+                    N_fail += 1
+        print('-----------------------------------------------')
+        print('all done, {} in total, {} success, {} fail. Have a nice day!\n'.format(N_datafile, N_success, N_fail))
+        #print('failed file:')
+    except:
+        print('Ooops, something went wrong...')
 
 if __name__ == "__main__":
-    right_input = False
+    exit = False
 
-    while not right_input:
+    while True:
         mode = input('\nSelect Mode: 1. Single file | 2. Batch mode (input 1 or 2): ')
         print()
         if mode == '1':
@@ -53,6 +56,5 @@ if __name__ == "__main__":
             batchMode()
         else:
             print('please input 1 or 2 !')
-    input('\npress ENTER to exit')
             
             
